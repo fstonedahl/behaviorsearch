b6e1aeb49d9cf3734b818dfc219d77d48367859c
annguyendang14 <annguyendang14@augustana.edu>
2016-03-01 15:54:34 -0600

    add action to Browse button, change line delimeter to UNIX
    I change my eclipse, hopefully it will not change the whole project
    anymore (this commit should change all file back to UNIX line)

--- src/bsearch/fx/MainController.java
+++ src/bsearch/fx/MainController.java
@@ -1,19 +1,29 @@
 package bsearch.fx;
 
 
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import bsearch.util.GeneralUtils;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 
 public class MainController extends Application implements Initializable {
+	@FXML public AnchorPane anchorPane;
+	@FXML public Button browseButton;
+	@FXML public TextField browseField;
 	
 
 	@Override
@@ -31,9 +41,9 @@ public class MainController extends Application implements Initializable {
 			Scene scene = new Scene(root);
 			primaryStage.setScene(scene);
 			primaryStage.show();
-			//Image icon = new Image(GeneralUtils.getResource("icon_behaviorsearch.png").getAbsolutePath());
-			//primaryStage.getIcons().add(icon);
-
+			Image icon = new Image(GeneralUtils.getResource("icon_behaviorsearch.png").toURI().toString());
+			primaryStage.getIcons().add(icon);
+			
 
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -45,5 +55,14 @@ public class MainController extends Application implements Initializable {
 		launch(args);
 
 	}
+	
+	public void browseFile(ActionEvent event){
+		FileChooser fileChooser = new FileChooser();
+		File selectedFile = fileChooser.showOpenDialog(null);
+		if (selectedFile != null) {
+			browseField.setText(selectedFile.getPath());
+			}
 
+
+	}
 }
