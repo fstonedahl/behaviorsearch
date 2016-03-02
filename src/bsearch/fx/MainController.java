abfcf52304b50503c2742a43ea80452923179233
annguyendang14 <annguyendang14@augustana.edu>
2016-03-02 13:41:31 -0600

    Add choice to ChoiceBox in SO tab

--- src/bsearch/fx/MainController.java
+++ src/bsearch/fx/MainController.java
@@ -12,23 +12,51 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import javafx.application.Application;
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 
 public class MainController extends Application implements Initializable {
+	// component outside of tab will have normal name
 	@FXML public AnchorPane anchorPane;
 	@FXML public Button browseButton;
 	@FXML public TextField browseField;
+	
+	// component in Search Objective tab will start with SO
+	@FXML public ChoiceBox<String> SOGoalBox;
+	@FXML public ChoiceBox<String> SOCollectedMearsureBox;
+	@FXML public ChoiceBox<String> SOFixedSamplingBox;
+	@FXML public ChoiceBox<String> SOCombineReplicatesBox;
+	@FXML public ChoiceBox<String> SOWrtBox;
+	
+	// component in Search Algorithm tab will start with SA
+	@FXML public ChoiceBox<String> SASearchMethodBox;
+	@FXML public ChoiceBox<String> SASearchEncodingBox;
 	
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		// TODO Auto-generated method stub
+		// set up ChoiceBox in SO tab
+		SOGoalBox.setItems(FXCollections.observableArrayList(
+		    "Minimize Fitness", "Maximize Fitness"));
+		SOCollectedMearsureBox.setItems(FXCollections.observableArrayList(
+			"AT_FINAL_STEP", "MEAN_ACROSS_STEPS", "MEDIAN_ACROSS_STEPS", 
+			"MIN_ACROSS_STEPS", "MAX_ACROSS_STEPS", "VARIANCE_ACROSS_STEPS",
+			"SUM_ACROSS_STEPS"));
+		SOFixedSamplingBox.setItems(FXCollections.observableArrayList("Fixed Sampling"));
+		SOCombineReplicatesBox.setItems(FXCollections.observableArrayList(
+			"MEAN", "MEDIAN", "MIN", "MAX", "VARIANCE", "STDEV"));
+		SOWrtBox.setItems(FXCollections.observableArrayList(
+			"integerParameter", "continuousParameter", "choiceParameter", "@MUTATE@"));
+		
+		//// set up ChoiceBox in SA tab
 		
 	}
 
