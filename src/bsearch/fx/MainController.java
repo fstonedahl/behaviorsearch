f3c16ea467323b0a7f2339af57613814fe502033
annguyendang14 <annguyendang14@augustana.edu>
2016-03-10 16:45:31 -0800

    working Param area and wrt ComboBox

--- src/bsearch/fx/MainController.java
+++ src/bsearch/fx/MainController.java
@@ -7,12 +7,16 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
 
+import javax.swing.DefaultComboBoxModel;
 import javax.swing.JOptionPane;
 
 import bsearch.algorithms.SearchMethodLoader;
 import bsearch.app.BehaviorSearchException;
 import bsearch.app.SearchProtocol;
+import bsearch.nlogolink.NetLogoLinkException;
 import bsearch.representations.ChromosomeTypeLoader;
+import bsearch.space.ParameterSpec;
+import bsearch.space.SearchSpace;
 import bsearch.util.GeneralUtils;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -20,7 +24,9 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
@@ -36,12 +42,19 @@ public class MainController extends Application implements Initializable {
 	@FXML public Button browseButton;
 	@FXML public TextField browseField;
 	
+	// component in Model tab will start with M
+	@FXML public TextArea MParamSpecsArea;
+	@FXML public Button MSuggestParamButton;
+
+	
 	// component in Search Objective tab will start with SO
 	@FXML public ChoiceBox<String> SOGoalBox;
 	@FXML public ChoiceBox<String> SOFitnessCollectingBox;
 	@FXML public ChoiceBox<String> SOFixedSamplingBox;
 	@FXML public ChoiceBox<String> SOCombineReplicatesBox;
 	@FXML public ChoiceBox<String> SOWrtBox;
+	@FXML public CheckBox SOTakeDerivativeCheckBox;
+	@FXML public TextField SODeltaField;
 	
 	// component in Search Algorithm tab will start with SA
 	@FXML public ChoiceBox<String> SASearchMethodBox;
@@ -73,8 +86,9 @@ public class MainController extends Application implements Initializable {
 		}
 		SOCombineReplicatesBox.setItems(FXCollections.observableArrayList(
 			combineReplication));
+		
 		SOWrtBox.setItems(FXCollections.observableArrayList(
-			"integerParameter", "continuousParameter", "choiceParameter", "@MUTATE@"));
+			"---"));
 		
 		//// set up ChoiceBox in SA tab
 		try {
@@ -116,7 +130,7 @@ public class MainController extends Application implements Initializable {
 		launch(args);
 
 	}
-	
+	//
 	public void browseFile(ActionEvent event){
 		FileChooser fileChooser = new FileChooser();
 		File selectedFile = fileChooser.showOpenDialog(null);
@@ -126,7 +140,33 @@ public class MainController extends Application implements Initializable {
 
 
 	}
+	//
+	public void takeDerivativeAction(ActionEvent event){
+		if (SOTakeDerivativeCheckBox.isSelected()){
+			this.SOWrtBox.setDisable(false);
+			this.SODeltaField.setDisable(false);
+			//TODO: Set Wrt box item after check the model param
+			List<String> wrt = new ArrayList<String>();
+			SearchSpace ss = new SearchSpace(java.util.Arrays.asList(this.MParamSpecsArea.getText().split("\n")));
+			
+			for (ParameterSpec spec : ss.getParamSpecs())
+			{
+				wrt.add(spec.getParameterName());
+			}
+			wrt.add("@MUTATE@");
+			this.SOWrtBox.setItems(FXCollections.observableArrayList(
+					wrt));
+		}
+	}
 	
+	public void suggestParam(ActionEvent event){
+		try {
+			this.MParamSpecsArea.setText(bsearch.nlogolink.Utils.getDefaultConstraintsText(this.browseField.getText()));
+		} catch (NetLogoLinkException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 	
 	//TODO: change to JavaFx dialog 
 	public static void handleError(String msg, java.awt.Container parentContainer)
